66120a8d092471430b6271010c118a3c0b773767
laplamgor <laplamgor@gmail.com>
2018-08-31 20:51:59 +0800

    Change settings

--- android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/rbgrnlivewallpaper/LessonThreeWallpaperService.java
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/rbgrnlivewallpaper/LessonThreeWallpaperService.java
@@ -3,10 +3,11 @@ package com.learnopengles.android.rbgrnlivewallpaper;
 import android.opengl.GLSurfaceView.Renderer;
 
 import com.learnopengles.android.lesson3.LessonThreeRenderer;
+import com.learnopengles.android.lesson6.LessonSixRenderer;
 
 public class LessonThreeWallpaperService extends OpenGLES2WallpaperService {
 	@Override
 	Renderer getNewRenderer() {
-		return new LessonThreeRenderer();
+		return new LessonSixRenderer(this);
 	}
 }
